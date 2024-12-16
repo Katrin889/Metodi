a9e84f9198a2181772dd0c418de785483fa43aa4
Katrin889 <kakamitreva@gmail.com>
2024-12-16 13:44:48 +0200

    Add files via upload

---
+++ Metodi/Metodi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodi
+{
+    internal class Program
+    {
+        static void Ime()
+        {
+            Console.Write("Name: ");
+            var a = Console.ReadLine();
+            Console.WriteLine("Hallo, " + a);
+        }
+        static void Main(string[] args)
+        {
+            Ime();
+        }
+    }
+}
---
+++ Metodi/zad 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_2
+{
+    internal class Program
+    {
+        static void x(int x, int y, int z)
+        {
+            x += 5;
+            y *= x;
+            z = y * 10;
+            Console.WriteLine("x=" + x);
+            Console.WriteLine("y=" + y);
+            Console.WriteLine("z=" + z);
+
+        }
+        static void Main(string[] args)
+        {
+            x(2, 3, 4);
+        }
+    }
+}
